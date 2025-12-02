40450b4fa3fd8ec560b7deba587afd73583cd824
Gabriel Cruz <gabriel.crzprz45@gmail.com>
2025-12-02 14:48:20 -0400

    finished with the 3rd exercise

--- supplementaryMaterial/javascript/javascript_advanced.js
+++ supplementaryMaterial/javascript/javascript_advanced.js
@@ -41,14 +41,58 @@ console.log(
 
 
 // Shallow vs. Deep Copy
-// Given a nested missionData object, use the spread
-// operator (...) to create a shallow copy. Then,
-// demonstrate a change to a nested property in the copy
-// still affects the original. Use
-// JSON.parse(JSON.stringify(obj)) to create a deep copy
+
+// Given a nested missionData object, use the spread operator (...) 
+// to create a shallow copy. Then, demonstrate a change to a nested property in the copy
+// still affects the original. Use JSON.parse(JSON.stringify(obj)) to create a deep copy
 // and show that changes are isolated.
 
+// shallow copies are dependent on source | deep copies are independent of source
+// const ingredientList = ["noodles", {list : ["eggs", "flour", "water"] }];
+// const ingredientListCopy = Array.from(ingredientList);
 
+// ingredientListCopy[1].list = ['rice flour', 'water'];
+
+// console.log(ingredientListCopy);    // [ 'noodles', { list: [ 'rice flour', 'water' ] } ]
+// console.log(ingredientList);        // [ 'noodles', { list: [ 'rice flour', 'water' ] } ]
+
+
+const missionData = {
+  id: "Tachikoma-01",
+  operation: "Innocence-Retrieval",
+  details: {
+    target: "Puppet Master",
+    location: "Niihama, Japan",
+    status: "Preparation",
+    team: ["Motoko", "Batou", "Togusa"]
+  },
+  timestamp: Date.now()
+};
+
+
+// shallow copy
+const shallowMissionData = {...missionData};
+
+// change values for check
+shallowMissionData['operation'] = "mofon-GO"
+shallowMissionData['details']['target'] = "Los Gorditos";
+
+// check if shallow copy
+console.log(missionData);
+console.log(shallowMissionData);
+
+
+
+// deep copy
+const deepMissionData = JSON.parse(JSON.stringify(missionData));
+
+// change values for check
+deepMissionData['operation'] = "Innocence_Retrieval";
+deepMissionData['details']['team'] = [...deepMissionData['details']['team'], "Gabs"];
+
+// check if deep copy
+console.log(missionData);
+console.log(deepMissionData);
 
 
 
